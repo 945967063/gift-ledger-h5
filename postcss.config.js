caf410c2362f775922094e76eaeda0fbd2b5f5c2
lihaha <linian@oo.local>
2026-08-12 16:07:14 +0800

    fix: 统一页面视口适配规则

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 export default {
   plugins: {
     tailwindcss: {},
+    'cnjm-postcss-px-to-viewport': {
+      viewportWidth: 375,
+      unitPrecision: 4,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      unitToConvert: 'px',
+      minPixelValue: 1,
+    },
     autoprefixer: {
       overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7'],
     },
